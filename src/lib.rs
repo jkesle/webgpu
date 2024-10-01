e2527254b60d3af02af98cc2c2259cb60e58e1dd
Josh Kesler <joshua.e.kesler@gmail.com>
2024-10-01 06:18:07 -0600

    target webgpu

--- src/lib.rs
+++ src/lib.rs
@@ -1,23 +1,28 @@
 use std::{env::var, path::PathBuf};
-use wgpu::{Instance, InstanceDescriptor, Backends, InstanceFlags, Dx12Compiler::Dxc, Gles3MinorVersion::Automatic, PowerPreference, RequestAdapterOptions};
-use wasm_bindgen::prelude::*;
-use web_sys::{Window, Document, Element, HtmlElement, Node, Gpu};
+use wgpu::{Instance, InstanceDescriptor, Backends, InstanceFlags, Dx12Compiler::Dxc, Gles3MinorVersion::Automatic, PowerPreference, RequestAdapterOptions, Adapter, Device, SurfaceTarget, WindowHandle};
+use wasm_bindgen::{JsCast, prelude::*};
+use web_sys::{Document, Element, Gpu, HtmlCanvasElement, HtmlElement, Navigator, Node, Window};
+use wasm_bindgen_futures::JsFuture;
 
 #[wasm_bindgen(start)]
-pub fn run() {
+pub async fn run() {
     let window: Window = web_sys::window().expect("no global window");
     let document: Document = window.document().expect("window should contain document object");
-    let body: HtmlElement = document.body().expect("body element should be present on document");
+    let canvas: HtmlCanvasElement = document.query_selector("#gpu-canvas").unwrap_throw().unwrap_throw().dyn_into::<HtmlCanvasElement>().expect("should be castable to HtmlCanvasElement");
 
-    let gpu: Gpu = window.navigator().gpu();
-    let adapter = gpu.request_adapter();
 
     let out_dir = var("OUT_DIR").unwrap();
-
     let instance = Instance::new(InstanceDescriptor {
-        backends: Backends::BROWSER_WEBGPU,
+        backends: Backends::PRIMARY,
         flags: InstanceFlags::DEBUG,
         dx12_shader_compiler: Dxc { dxil_path: Some(PathBuf::from(&out_dir).join("dxc")), dxc_path: Some(PathBuf::from(&out_dir).join("dxc")) },
         gles_minor_version: Automatic
     });
+
+    let surface_target = SurfaceTarget::Canvas(canvas);
+    let surface = instance.create_surface(surface_target).unwrap(); // Handle this better than with unwrap
+
+    let adapter: Adapter = instance.request_adapter(&RequestAdapterOptions { power_preference: PowerPreference::HighPerformance, force_fallback_adapter: false, compatible_surface: Some(&surface) }).await.unwrap_throw();
+
+    todo!("Grab device from adapter");
 }
